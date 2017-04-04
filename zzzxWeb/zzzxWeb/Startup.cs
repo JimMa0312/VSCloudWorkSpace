c7d247905f4fa0be58b89a506306c5f3eb922d4e
JimMa <JimMa0312@outlook.com>
2017-04-05 01:22:46 +0800

    创建项目

---
+++ zzzxWeb/zzzxWeb/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(zzzxWeb.Startup))]
+namespace zzzxWeb
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
